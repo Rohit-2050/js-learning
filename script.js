2423ed8dae23458bf0f582356f7424a899e8a0b9
Rohit M <rohit.m.sde1@gmail.com>
2025-08-13 22:12:19 +0530

    arrays and objects

--- script.js
+++ script.js
@@ -56,3 +56,59 @@ console.log(bio.toLowerCase());
 console.log(bio.includes("goat"));
 console.log(bio.slice(0,9));
 console.log(bio.replace("goat","legend"));
+
+let str = " I am GOAT";
+console.log(str.trim().toLowerCase().replace("goat","legend").includes("legend"));
+
+// numbers 
+let num = " 56.789 ";
+// let result = num.trim().isInteger();
+// console.log(result);
+// console.log(num);
+// let rounded = num.toFixed(2);
+// console.log(rounded);
+
+//math
+// let ran = Math.floor(Math.random()* (50 -1 +1)+1);
+// console.log(ran);
+// if(ran % 5 === 0){
+//       return "multiple of 2";
+// }
+// else{
+//       return "not a multiple";
+// }
+// array
+const arr1 = [1,2,3];
+console.log(arr1);
+arr1.push(4);
+console.log(arr1);
+arr1.pop();
+console.log(arr1);
+arr1.unshift(0);
+console.log(arr1);
+arr1.shift();
+console.log(arr1);
+
+let marks = [67.5, 89.2, 45.3, 78, 92.6];
+console.log(marks.length);
+console.log(marks[0]);
+console.log(marks[-1]);
+let sum = 0;
+let avg = 0;
+for(let i =0;i<marks.length;i++){
+    marks[i] = Math.floor(marks[i]);
+      sum += marks[i];
+      console.log(sum);
+      avg = sum / marks.length
+      console.log(avg.toFixed(2));
+}
+
+const student = {
+  name: "Riya",
+  age: 21,
+  skills: ["HTML", "CSS", "JavaScript"]
+};
+
+student.skills.push("bootstrap");
+console.log(student.skills[skills.length -1 ]);
+student.skills.replace("css","tailwind");
